6e51a89fcdfb88ddd437928a102f0a7313666da7
Grin <asusgrin@gmail.com>
2021-09-17 09:31:46 +0300

    Create PowerBI.py

---
+++ PowerBI.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Скрипт для программы визуализации данных POWER BI
+# API позволяет получить информацию о наличных курсах валют ПриватБанка и НБУ на текущюю дату или выбранную дату. 
+# Архив хранит данные за последние 4 года
+#API = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
+
+
+# импортируем необходимые библиотеки
+import time
+import requests
+import pandas as pd
+
+# получаем текущюю дату
+day = time.strftime("%d.%m.%Y")
+
+# подставляем дату и получаем данные из API приватбанка в формате JSON
+API2 = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + day
+
+result = requests.get(API2)
+res = result.json()
+
+# формируем датафрейм
+exchange = pd.DataFrame(res)
+print(exchange)
+    
+
